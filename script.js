51a0659da8b1f418e6cc8c9ecf4499e46c2d7059
Aiden Stone <[176074820+BlockyHead06@users.noreply.github.com]>
2024-09-24 21:01:35 -0400

    Return appropriate string depending on randNum
    If randNum is 0, will return "rock"
    If randNum is 1, will return "paper"
    If randNum is 2, will return "scissors"

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function getComputerChoice() {
     // Get a random number from 0 - 2
     let randNum = Math.floor(Math.random() * 3);
-    // If the number is 0, return "rock"
-    // If the number is 1, return "paper"
-    // If the number is 2, return "scissors"
+
+    switch (randNum) {
+        case 0: // If the number is 0, return "rock"
+            return "rock"; 
+        case 1: // If the number is 1, return "paper"
+            return "paper";
+        case 2: // If the number is 2, return "scissors"
+            return "scissors";
+    }
 }
