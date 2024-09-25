7ce310d62de06af71d46a5fd31ec6ddc9b416fff
Aiden Stone <[176074820+BlockyHead06@users.noreply.github.com]>
2024-09-24 20:47:53 -0400

    Add getComputerChoice function with pseudocode

---
+++ script.js
@@ -0,0 +1,6 @@
+function getComputerChoice() {
+    // Get a random number between 0 and 2
+    // If the number is 0, return "rock"
+    // If the number is 1, return "paper"
+    // If the number is 2, return "scissors"
+}
