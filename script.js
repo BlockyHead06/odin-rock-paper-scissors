5aa51d88d27010b847c115273b7d516f345bfc0a
Aiden Stone <[176074820+BlockyHead06@users.noreply.github.com]>
2024-09-24 20:56:40 -0400

    Set randNum variable

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function getComputerChoice() {
-    // Get a random number between 0 and 2
+    // Get a random number from 0 - 2
+    let randNum = Math.floor(Math.random() * 3);
     // If the number is 0, return "rock"
     // If the number is 1, return "paper"
     // If the number is 2, return "scissors"
