1110a02b1e2f306bdfabbf9a75a6b291fdb629c8
Aiden Stone <[176074820+BlockyHead06@users.noreply.github.com]>
2024-12-23 14:40:23 -0500

    Display game results in results div

--- script.js
+++ script.js
@@ -33,21 +33,22 @@ function getHumanChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+let results = document.getElementById("results");
 
 function playRound(humanChoice, computerChoice) {
     // Determine and output the winner
     if (humanChoice === computerChoice) {
-        console.log("It's a tie!");
+        results.innerText = "It's a tie!";
     }
     else if (humanChoice === "rock" && computerChoice === "scissors"
         || humanChoice === "paper" && computerChoice === "rock"
         || humanChoice === "scissors" && computerChoice === "paper") {
-        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        results.innerText = `You win, ${humanChoice} beats ${computerChoice}!`;
         // If human wins, increment humanScore
         ++humanScore;
     }
     else {
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        results.innerText = `You lose, ${computerChoice} beats ${humanChoice}!`;
         // If computer wins, increment computerScore
         ++computerScore;
     }
